af0fca135f63d1277d5da14e4c295f6171629769
hyloong <730018817@qq.com>
2016-09-02 07:46:56 +0800

    test linuxC

--- fib.c
+++ fib.c
@@ -10,6 +10,30 @@ int fib(int n)
     return fib(n-1)+fib(n-2);
 }
 
+//
+void show_int_type(){
+    int i;
+    short int si;
+    long int li;
+    int a, b, c;
+    a = sizeof(i);
+    b = sizeof(si);
+    c = sizeof(li);
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+}
+
+void int_2_char(){
+    int c1, c2;
+    char c3;
+    c1 = 'a'-'A';
+    c2 = 'b'-'B';
+    c3 = 'c'-32;
+    printf("c1 is %d and c2 is %d \n", c1, c2);
+    printf("c1 is %d and c3 is %c \n", c3, c3);
+}
+
 int main()
 {
     int n, f, s;
@@ -17,7 +41,36 @@ int main()
     scanf("%d", &n);
     f = fib(n);
     printf("f=%d\n", f);
+    // max
     s = MAX(n, f);
     printf("s=%d\n", s);
+
+    show_int_type();
+
+    int_2_char();
     return 0;
 }
+
+
+adasd
+dddddydddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddydddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxdddyxxxdddyx
+
+ddyxyx
+ddyxyx
+ddyxyx
+ddyxyx
+
+ddyxyx
+dyx
+dyx
+
+dyx:
+
+
+
+ddasdasdasd asdasd aas
+
+asdas
+
+
+
